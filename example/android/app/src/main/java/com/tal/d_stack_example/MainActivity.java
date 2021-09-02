adb4e904115980c5c8ddeb2edcc62e5435368814
Sunbreak <sunbreak.wang@gmail.com>
2021-09-02 16:09:49 +0800

    feat: Add native entry page on Android
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

--- example/android/app/src/main/java/com/tal/d_stack_example/MainActivity.java
+++ example/android/app/src/main/java/com/tal/d_stack_example/MainActivity.java
@@ -1,6 +1,32 @@
 package com.tal.d_stack_example;
 
-import io.flutter.embedding.android.FlutterActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends FlutterActivity {
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("MainActivity");
+
+        findViewById(R.id.push_flutter_1).setOnClickListener((View v) -> {
+            System.out.println("push_flutter_1"); // TODO
+        });
+
+        findViewById(R.id.push_flutter_2).setOnClickListener((View v) -> {
+            System.out.println("push_flutter_2"); // TODO
+        });
+
+        findViewById(R.id.push_native_1).setOnClickListener((View v) -> {
+            System.out.println("push_native_1"); // TODO
+        });
+
+        findViewById(R.id.push_native_2).setOnClickListener((View v) -> {
+            System.out.println("push_native_2"); // TODO
+        });
+    }
 }
